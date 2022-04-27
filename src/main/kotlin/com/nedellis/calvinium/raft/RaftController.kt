6fdd51046061929f5b87413b2346e7fac258f17c
epellis <nedellis1234@gmail.com>
2022-04-26 23:56:22 -0700

    Before implementing membership

--- src/main/kotlin/com/nedellis/calvinium/raft/RaftController.kt
+++ src/main/kotlin/com/nedellis/calvinium/raft/RaftController.kt
@@ -1,12 +1,21 @@
 package com.nedellis.calvinium.raft
 
 import com.google.common.util.concurrent.AbstractExecutionThreadService
+import com.nedellis.calvinium.proto.AppendEntriesRequest
+import com.nedellis.calvinium.proto.AppendEntriesResponse
 import com.nedellis.calvinium.proto.RaftGrpcKt
+import java.util.UUID
 
-class RaftController : AbstractExecutionThreadService() {
+class RaftController(id: UUID) : AbstractExecutionThreadService() {
+    private val raftState = buildRaftStateMachine(id)
+
     override fun run() {
         TODO("Not yet implemented")
     }
 }
 
-class RaftServer : RaftGrpcKt.RaftCoroutineImplBase() {}
+class RaftServer : RaftGrpcKt.RaftCoroutineImplBase() {
+    override suspend fun appendEntries(request: AppendEntriesRequest): AppendEntriesResponse {
+        return super.appendEntries(request)
+    }
+}
