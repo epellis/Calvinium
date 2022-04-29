9e8fe734d55decb07c7d0dc23fc982d1744cb7f5
epellis <nedellis1234@gmail.com>
2022-04-28 21:56:07 -0700

    WIP

--- src/main/kotlin/com/nedellis/calvinium/swim/Swim.kt
+++ src/main/kotlin/com/nedellis/calvinium/swim/Swim.kt
@@ -1,8 +1,20 @@
 package com.nedellis.calvinium.swim
 
+import com.google.common.util.concurrent.AbstractExecutionThreadService
+import com.nedellis.calvinium.proto.SwimGossip
+import com.nedellis.calvinium.proto.swimGossip
 import com.tinder.StateMachine
+import io.grpc.Channel
+import io.grpc.ManagedChannelBuilder
 import java.time.Duration
 import java.util.UUID
+import kotlin.random.Random
+import kotlin.time.toKotlinDuration
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import mu.KotlinLogging
 
 internal val FAILURE_DETECTOR_PERIOD: Duration = Duration.ofMinutes(10)
 internal const val FAILURE_DETECTOR_SUBGROUP_SIZE: Int = 1
@@ -64,6 +76,57 @@ internal fun buildArbitrarySwimPeerStateMachine(
             val validTransition = it as? StateMachine.Transition.Valid ?: return@onTransition
             when (validTransition.sideEffect) {
                 else -> {}
+            }
+        }
+    }
+}
+
+internal data class SwimServiceState(
+    val otherMembers: Map<UUID, StateMachine<PeerState, Event, SideEffect>>
+) {
+    private val logger = KotlinLogging.logger {}
+}
+
+internal class SwimService : AbstractExecutionThreadService() {
+    private val logger = KotlinLogging.logger {}
+
+    private val lock = Mutex()
+    private val state = SwimServiceState(otherMembers = mapOf())
+
+    private var connections = mapOf<UUID, Channel>()
+
+    private fun buildGossip(): SwimGossip {
+        return swimGossip {}
+    }
+
+    private fun connectionForId(id: UUID): Channel {
+        val conn =
+            connections.getOrElse(id) {
+                ManagedChannelBuilder.forAddress("", 0).usePlaintext().build()
+            }
+
+        connections = connections.plus(id to conn)
+        return conn
+    }
+
+    override fun run() {
+        runBlocking {
+            while (isRunning) {
+                delay(FAILURE_DETECTOR_PERIOD.toKotlinDuration())
+                lock.withLock {
+                    val livePeers = state.otherMembers.filterValues { it.state is PeerState.Alive }
+
+                    if (livePeers.isNotEmpty()) {
+                        val (id, state) = livePeers.toList()[Random.nextInt(livePeers.size - 1)]
+                        val rpc =
+                            state.transition(Event.PingMember) as
+                                StateMachine.Transition.Valid<*, *, *>
+                    } else {
+                        logger.debug {
+                            "Skipping broadcast, no state in ${state.otherMembers.entries} was live"
+                        }
+                    }
+                }
             }
         }
     }
